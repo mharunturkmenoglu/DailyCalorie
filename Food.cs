e07aba4c93a226f73498d385cb992a114d8b7b46
mahmutkiliic <35044943+mahmutkiliic@users.noreply.github.com>
2022-06-08 22:41:58 +0300

    sql added
    sql added

---
+++ Food.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DailyCalorie
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        public int Calorie { get; set; }
+    }
+}
