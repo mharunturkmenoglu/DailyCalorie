2d2a01486c83c2bbcbd2a56ca5929a03a4a6e06e
Yigithan Guven <demirejderha@gmail.com>
2022-06-09 23:25:19 +0300

    feat: Added calorie counter

--- Calorie.aspx.cs
+++ Calorie.aspx.cs
@@ -9,23 +9,72 @@ namespace DailyCalorie
         Context context = new Context();
         protected void Page_Load(object sender, EventArgs e)
         {
-            List<Food> foodList = new List<Food>();
-            List<User> userList = new List<User>();
-
-            foodList = context.GetAllFood();
-            userList = context.GetAllUsers();
-
-           
-
-
+            calorietext.Visible = false;
         }
 
         protected void calculateButton_Click(object sender, EventArgs e)
         {
-           string x= weighttextbox.Value;
-            x= weighttextbox.Value.ToString();
             
+            int age = Int32.Parse(ageTextbox.Text);
+            int weightKg = Int32.Parse(weightTextbox.Text);
+            int heightSm = Int32.Parse(heightTextbox.Text);
+
+            int gender = genderListbox.SelectedIndex;
+            int workoutsPerWeek = workoutsListBox.SelectedIndex;
             
+            decimal BMR =CalculateBMR(age,weightKg,heightSm,gender);
+            decimal DCI=CalculateDCI(BMR,workoutsPerWeek);
+
+            calorietext.Visible=true;
+            calorietext.InnerText = "Your Daily Calorie Is: " + Math.Floor(DCI).ToString();
+
+            Session["Calorie"] = Math.Floor(DCI);
+        }
+
+        private decimal CalculateDCI(decimal BMR , int workoutsPerWeek)
+        {
+            decimal DCI = 0;
+            if (workoutsPerWeek == 0)
+            {
+                DCI = (BMR * 1.2m);
+            }
+            else if (workoutsPerWeek == 1)
+            {
+                DCI = (BMR * 1.375m);
+            }
+            else if (workoutsPerWeek == 2)
+            {
+                DCI = (BMR * 1.55m);
+            }
+            else if (workoutsPerWeek == 3)
+            {
+                DCI = (BMR * 1.725m);
+            }
+            else if (workoutsPerWeek == 4)
+            {
+                DCI = (BMR * 1.9m);
+            }
+
+            return DCI;
+        }
+
+        private decimal CalculateBMR(int age , int weightKg ,int heightSm,int gender)
+        {
+            decimal BMR = 0;
+            if (gender == 0)
+            {
+                BMR = (66.5m + (13.75m * weightKg) + (5.003m * heightSm) - (6.755m * age));
+            }
+            else if (gender == 1)
+            {
+                BMR = (655 + (9.563m * weightKg) + (1.850m * heightSm) - (4.676m * age));
+            }
+            return BMR;
+        }
+
+        protected void backButton_Click(object sender, EventArgs e)
+        {
+            //TODO : Go back to home screen
         }
     }
 }
--- Calorie.aspx.designer.cs
+++ Calorie.aspx.designer.cs
@@ -24,22 +24,31 @@ namespace DailyCalorie
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// weighttextbox control.
+        /// weightTextbox control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputText weighttextbox;
+        protected global::System.Web.UI.WebControls.TextBox weightTextbox;
 
         /// <summary>
-        /// heighttextbox control.
+        /// heightTextbox control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputText heighttextbox;
+        protected global::System.Web.UI.WebControls.TextBox heightTextbox;
+
+        /// <summary>
+        /// ageTextbox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ageTextbox;
 
         /// <summary>
         /// genderListbox control.
